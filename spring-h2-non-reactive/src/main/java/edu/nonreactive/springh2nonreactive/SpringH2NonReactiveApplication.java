62e5e5567f65bbc9df6fd0a91fe5605250d32210
holowizor <marek.kwiecien@gmail.com>
2020-08-11 11:41:50 +0200

    project template for boot and jpa

---
+++ spring-h2-non-reactive/src/main/java/edu/nonreactive/springh2nonreactive/SpringH2NonReactiveApplication.java
@@ -0,0 +1,13 @@
+package edu.nonreactive.springh2nonreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringH2NonReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringH2NonReactiveApplication.class, args);
+	}
+
+}
---
+++ spring-h2-non-reactive/src/test/java/edu/nonreactive/springh2nonreactive/SpringH2NonReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nonreactive.springh2nonreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringH2NonReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
